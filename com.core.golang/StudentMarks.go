b089cca6e0f5858181145397a14270aed0e56ff9
Pooja Jaiswar <pooja.jaiswar.987@gmail.com>
2025-10-31 16:49:02 +0530

    GOLang: StudentMarks

--- com.core.golang/StudentMarks.go
+++ com.core.golang/StudentMarks.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
-
-	"Core/com.core.golang"
 )
+
+type Student struct {
+	RollNo int
+	Name   string
+	Marks  int
+}
+
+func (s *Student) GetResults() (string, error) {
+	if s.Marks < 0 {
+		return "", fmt.Errorf("invalid Marks for student %s", s.Name)
+	}
+	if s.Marks >= 40 {
+		return "Pass", nil
+	}
+	return "Fail", nil
+}
 
 func main() {
 
-	s1 := com_core_golang.Student{RollNo: 01, Name: "Pooja", Marks: 80}
-	s2 := com_core_golang.Student{RollNo: 21, Name: "Durga", Marks: 90}
+	s1 := Student{RollNo: 01, Name: "Pooja", Marks: 80}
+	s2 := Student{RollNo: 21, Name: "Durga", Marks: 90}
 
 	rest1, _ := s1.GetResults()
 	rest2, _ := s2.GetResults()
